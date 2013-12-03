8a8a3ecc3b2e363b6c9fc752c487e4f6c5df1f01
Daniel Marino <starzonmyarmz@gmail.com>
2013-12-02 19:04:04 -0500

    More scafolding and template and assets

--- web.rb
+++ web.rb
@@ -9,11 +9,12 @@ rdio = Rdio.new([RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET])
 set :public, File.dirname(__FILE__) + '/static'
 
 get '/' do
-  # redirect '/main.html'
-  # albums = rdio.call('getAlbumsInCollection', { 'type' => 'Artist' })
+  erb :index
 end
 
 get '/user/:vanityName' do |vanityName|
   content_type 'application/json', :charset => 'utf-8'
   return rdio.call('findUser', { :vanityName => vanityName }).to_json
 end
+
+# albums = rdio.call('getAlbumsInCollection', { 'type' => 'Artist' })
