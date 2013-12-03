1b88742ecad41205971a60ff4e90301aabcaa1cc
Daniel Marino <starzonmyarmz@gmail.com>
2013-12-02 20:26:36 -0500

    More play counts

--- static/application.js
+++ static/application.js
@@ -9,14 +9,27 @@
       var key = data.result.key;
 
       $.getJSON('/albums/' + key, function(data) {
-        var html = '';
+        var html      = '',
+            result    = data.result
 
         console.log(data)
 
-        for (var i = 0, len = data.result.length; i < len; i++ ) {
-          if (data.result[i].releaseDate.indexOf(year) === 0) {
-            html += '<li>' + data.result[i].name + '</li>';
+        for (var i = 0, len = result.length; i < len; i++ ) {
+
+          var playCount = 0;
+
+          if (result[i].releaseDate.indexOf(year) === 0) {
+
+            var counts    = '';
+
+            for (var y = 0, len = result[i].tracks.length; y < len; y++) {
+              playCount += result[i].tracks[y].playCount;
+              counts += '<li>' + result[i].tracks[y].name + ' (' + result[i].tracks[y].playCount + ')</li>';
+            }
+
+            html += '<li>' + result[i].name + ' (' + playCount + ')<ul>' + counts + '</ul></li>';
           }
+
         }
 
         if (html) {
