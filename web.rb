da17e9d34cb0b6b6dc0f8e91553db837901ac21d
Daniel Marino <starzonmyarmz@gmail.com>
2014-01-26 15:45:19 -0500

    Remove a bunch of unncessary attributes to speed things up

--- web.rb
+++ web.rb
@@ -15,16 +15,19 @@ get '/user/:vanityName' do |vanityName|
   return rdio.call('findUser', { :vanityName => vanityName })['result'].to_json
 end
 
-get '/albums/:key/:page' do |key, page|
-  content_type 'application/json', :charset => 'utf-8'
-  return rdio.call('getAlbumsInCollection', { :user => key, :sort => 'playCount', :count => 20, :extras => 'tracks,Track.playCount', :start => 20 * page.to_i })['result'].to_json
-end
-
-# This should probably replace the call to /albums eventually
 get '/popular/:key/:year/:page' do |key, year, page|
   content_type 'application/json', :charset => 'utf-8'
-  pop = rdio.call('getAlbumsInCollection', { :user => key, :sort => 'playCount', :count => 25, :start => 25 * page.to_i, :extras => 'bigIcon' })['result']
-  # Not using because it messes with pagination
-  # return pop.select { |s| s['releaseDate'].match year }.to_json
-  return pop.to_json
+
+  pop = rdio.call('getAlbumsInCollection', {
+    :user => key,
+    :sort => 'playCount',
+    :count => 100,
+    :start => 100 * page.to_i,
+    # Add bigIcon and remove as many unnecessary
+    # attributes as necessary to speed things up
+    :extras => 'bigIcon,-artistKey,-albumKey,-albumUrl,-artistUrl,-url,-price,-embedUrl,-icon,-baseIcon,-canSample,-duration,-userKey,-type,-key,-userName,-canSample,-length,-canTether,-rawArtistKey,-trackKeys,-itemTrackKeys' }
+  )['result']
+
+  # Only return those from the matched year
+  return pop.select { |s| s['releaseDate'].match year }.to_json
 end
